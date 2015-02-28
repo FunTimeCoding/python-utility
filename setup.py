17d271605abd01deec415220d54fb8025d7957ec
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-28 19:54:38 +0100

    unify setup.py format

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='python-utility',
-      version='0.1',
-      description='Stub description for python-utility.',
-      scripts=['bin/pu'],
-      packages=['python_utility'],
-      author='Alexander Reitzel',
-      author_email='funtimecoding@gmail.com',
-      url='http://example.org',
-      download_url='http://example.org/python-utility.tar.gz')
+setup(
+    name='python-utility',
+    version='0.1',
+    description='Stub description for python-utility.',
+    scripts=['bin/pu'],
+    packages=['python_utility'],
+    author='Alexander Reitzel',
+    author_email='funtimecoding@gmail.com',
+    url='http://example.org',
+    download_url='http://example.org/python-utility.tar.gz'
+)

7fc6fee1964acae44ce885cf631d1a13682c5986
Alexander Reitzel <funtimecoding@gmail.com>
2015-08-26 23:00:50 +0200

    add example code for a console interface

---
+++ python_utility/console_interface.py
@@ -0,0 +1,53 @@
+import curses
+from curses.textpad import rectangle, Textbox
+
+
+class ConsoleInterface:
+    def __init__(self, screen):
+        self.screen = screen
+
+    def start(self):
+        position_y = 0
+        position_x = 0
+        message = 'ctrl-g to send message'
+        self.screen.addstr(position_y, position_x, message)
+
+        try:
+            self.loop()
+        except KeyboardInterrupt:
+            pass
+
+    def loop(self):
+        # stdscr.clear()
+        # stdscr.getkey()
+
+        while True:
+            height = 10
+            width = 40
+            position_y = 2
+            position_x = 1
+            window = curses.newwin(height, width, position_y, position_x)
+
+            position_y = 1
+            position_x = 0
+            rectangle(
+                self.screen,
+                position_y,
+                position_x,
+                height + 2,
+                width + 1
+            )
+
+            self.screen.refresh()
+
+            box = Textbox(window)
+            box.edit()
+            message = box.gather()
+
+            position_y = 13
+            position_x = 0
+            self.screen.addstr(position_y, position_x, 'read: ' + message)
+            message = message.strip()
+
+            if message == 'quit':
+                break

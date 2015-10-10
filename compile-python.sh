0b9bbe05cf9ac4572957edcff1a5d154992f003e
Alexander Reitzel <funtimecoding@gmail.com>
2015-10-10 14:10:10 +0200

    merge changes from skeleton project

--- compile-python.sh
+++ compile-python.sh
@@ -1,18 +1,44 @@
-#!/bin/sh
+#!/bin/sh -e
+# To uninstall, delete the PREFIX directory.
 
-# On Debian Wheezy systems, make sure you're on the staff group.
-# To uninstall, simply delete the PREFIX directory.
-export VERSION=3.4.3
-export NAME=Python-${VERSION}
-export FILE=${NAME}.tgz
-export PREFIX=/usr/local/opt/python-${VERSION}
+USER_ID=$(id -u)
+
+if [ ! "${USER_ID}" = "0" ]; then
+    NOT_IN_STAFF=false
+    groups | grep staff > /dev/null || NOT_IN_STAFF=true
+
+    if [ "${NOT_IN_STAFF}" = true ]; then
+        echo "You must be in the staff group."
+
+        exit 1
+    fi
+fi
+
+VERSION=3.4.3
+NAME=Python-${VERSION}
+FILE=${NAME}.tgz
 cd /tmp
-wget https://www.python.org/ftp/python/${VERSION}/${FILE}
-tar -zxf /tmp/${FILE}
-cd ${NAME}
-./configure --prefix=${PREFIX}
-make
-make install
-rm /tmp/${FILE}
-rm -rf /tmp/${NAME}
-cd
+
+if [ ! -f "${FILE}" ]; then
+    wget https://www.python.org/ftp/python/${VERSION}/${FILE}
+fi
+
+if [ ! -d "${NAME}" ]; then
+    tar -zxf /tmp/${FILE}
+fi
+
+PREFIX=/usr/local/opt/python-${VERSION}
+
+if [ ! -d "${PREFIX}" ]; then
+    cd "${NAME}"
+    ./configure --prefix=${PREFIX}
+    make
+    make install
+fi
+
+if [ "${1}" = "--clean" ]; then
+    rm /tmp/${FILE}
+    rm -rf /tmp/${NAME}
+fi
+
+echo "Done."
--- run-style-check.sh
+++ run-style-check.sh
@@ -5,39 +5,37 @@ CI_MODE=0
 if [ "${1}" = "--ci-mode" ]; then
     shift
     CI_MODE=1
+    mkdir -p build/log
 fi
 
 echo "================================================================================"
-echo ""
+echo
 echo "Running PEP8."
 
 if [ "${CI_MODE}" = "1" ]; then
-    mkdir -p build/log
     pep8 --exclude=.git,.pyvenv,__pycache__ --statistics . | tee build/log/pep8.txt || true
 else
     pep8 --exclude=.git,.pyvenv,__pycache__ --statistics . || true
 fi
 
-echo ""
+echo
 echo "================================================================================"
-echo ""
+echo
 echo "Running PyLint."
 FIND=$(find . -name '*.py' -and -not -path '*/.pyvenv/*')
 
 if [ "${CI_MODE}" = "1" ]; then
-    mkdir -p build/log
     # TODO: Fix this warning. It's tricky. Adding quotes will break pylint.
     # shellcheck disable=SC2086
     pylint --rcfile=.pylintrc ${FIND} | tee build/log/pylint.txt
 else
-    # TODO: Fix this warning too.
     # shellcheck disable=SC2086
     pylint --rcfile=.pylintrc ${FIND}
 fi
 
 echo "================================================================================"
-echo ""
+echo
 echo "Running ShellCheck."
 find . -name '*.sh' -exec sh -c 'shellcheck ${1} || true' '_' '{}' \;
-echo ""
+echo
 echo "================================================================================"

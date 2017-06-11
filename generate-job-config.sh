64714c50a16b467d00daaed050dcf79cb65eacd6
Alexander Reitzel <funtimecoding@gmail.com>
2017-06-11 20:32:07 +0200

    update job config

--- generate-job-config.sh
+++ generate-job-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 # shellcheck disable=SC2016
-jjm --locator https://github.com/FunTimeCoding/python-utility.git --build-command ./build.sh > job.xml
+jjm --locator https://github.com/FunTimeCoding/python-utility.git --build-command ./build.sh --junit build/junit.xml > job.xml

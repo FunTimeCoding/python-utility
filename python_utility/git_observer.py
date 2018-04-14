07643023421a97b6116bb455766636607f6bb87a
Alexander Reitzel <funtimecoding@gmail.com>
2018-04-14 17:13:13 +0200

    fix style concerns

--- python_utility/git_observer.py
+++ python_utility/git_observer.py
@@ -1,3 +1,4 @@
-# TODO: Fetch master branch of a repository repeatedly and detect merges into master.
+# TODO: Fetch master branch of a repository repeatedly and detect merges
+# into master.
 # TODO: Detect tag creation.
 # TODO: Detect feature branch creation.

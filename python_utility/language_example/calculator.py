d7f211d362267dab523214a94ae2a2e97e278db6
Alexander Reitzel <funtimecoding@gmail.com>
2020-05-11 02:16:47 +0200

    move language example here from python-skeleton

---
+++ python_utility/language_example/calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+    @staticmethod
+    def add(augend: int, addend: int) -> int:
+        return augend + addend
+
+    @staticmethod
+    def subtract(minuend: int, subtrahend: int) -> int:
+        return minuend - subtrahend

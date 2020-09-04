8647c08159dfe238ce2cbcfa127030ebc1899038
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-04 22:22:21 +0200

    uncomment code, put into methods

--- python_utility/spreadsheet.py
+++ python_utility/spreadsheet.py
@@ -9,6 +9,8 @@ from google.auth.transport.requests import Request
 
 from python_utility.configuration import Configuration
 
+TOKEN_FILE = 'token.pickle'
+
 
 class Spreadsheet:
     @staticmethod
@@ -26,8 +28,8 @@ class Spreadsheet:
 
         credentials = None
 
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
+        if os.path.exists(TOKEN_FILE):
+            with open(TOKEN_FILE, 'rb') as token:
                 credentials = pickle.load(token)
 
         if not credentials or not credentials.valid:
@@ -41,24 +43,32 @@ class Spreadsheet:
                     ['https://www.googleapis.com/auth/spreadsheets']
                 )
                 credentials = flow.run_local_server(port=0)
-            with open('token.pickle', 'wb') as token:
+            with open(TOKEN_FILE, 'wb') as token:
                 pickle.dump(credentials, token)
 
         service = build('sheets', 'v4', credentials=credentials)
 
         sheet = service.spreadsheets()
-        # result = sheet.values().get(
-        #     spreadsheetId=identifier,
-        #     range='A1:B2'
-        # ).execute()
-        # values = result.get('values', [])
-        #
-        # if not values:
-        #     print('No data found.')
-        # else:
-        #     for row in values:
-        #         print('%s, %s' % (row[0], row[1]))
 
+        # Spreadsheet.update(sheet, identifier)
+        Spreadsheet.read(sheet, identifier)
+
+    @staticmethod
+    def update(sheet, identifier):
+        result = sheet.values().get(
+            spreadsheetId=identifier,
+            range='A1:B2'
+        ).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+        else:
+            for row in values:
+                print('%s, %s' % (row[0], row[1]))
+
+    @staticmethod
+    def read(sheet, identifier):
         sheet.values().update(
             spreadsheetId=identifier,
             valueInputOption='USER_ENTERED',

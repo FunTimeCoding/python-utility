b8b0cfbe212aad69dc33f04ff88b47a1761acafb
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-14 01:16:57 +0200

    test failing command

--- tests/test_command_process.py
+++ tests/test_command_process.py
@@ -1,10 +1,10 @@
-from python_utility.command_process import CommandProcess
+import pytest
+
+from python_utility.command_process import CommandProcess, CommandFailed
 
 
 def test_command_process(capfd) -> None:
-    process = CommandProcess(
-        arguments=['echo', 'hello']
-    )
+    process = CommandProcess(arguments=['echo', 'hello'])
 
     assert process.get_return_code() == 0
     assert process.get_standard_output() == 'hello'
@@ -14,3 +14,8 @@ def test_command_process(capfd) -> None:
     standard_output, standard_error = capfd.readouterr()
     assert standard_output == 'hello\n'
     assert standard_error == ''
+
+
+def test_command_fails() -> None:
+    with pytest.raises(CommandFailed):
+        CommandProcess(arguments=['ls', 'does-not-exist'])

55fc0e3ff4265a1145bb86227d46e208e7fb4efd
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-14 00:07:49 +0200

    create subdirectory in tmp

--- tests/powerline/test_vagrant.py
+++ tests/powerline/test_vagrant.py
@@ -1,23 +1,37 @@
-import os
+from os import mkdir, rmdir, getcwd
+from os.path import join, exists
 
 import pytest
 
 from python_utility.command_process import CommandFailed
 from python_utility.powerline.vagrant import VagrantSegment
 
+TEMPORARY_DIRECTORY = '/tmp/python_utility'
+
+
+def test_create_test_directory() -> None:
+    mkdir(TEMPORARY_DIRECTORY)
+    assert exists(TEMPORARY_DIRECTORY)
+
 
 def test_vagrant_file_exists() -> None:
-    assert VagrantSegment.vagrant_file_exists(os.getcwd())
-    assert not VagrantSegment.vagrant_file_exists('/tmp')
+    assert VagrantSegment.vagrant_file_exists(getcwd())
+    assert not VagrantSegment.vagrant_file_exists(TEMPORARY_DIRECTORY)
 
 
 def test_vagrant_directory_exists() -> None:
-    os.mkdir('/tmp/.vagrant')
-    assert VagrantSegment.vagrant_directory_exists('/tmp')
-    os.rmdir('/tmp/.vagrant')
-    assert not VagrantSegment.vagrant_directory_exists('/tmp')
+    vagrant_directory = join(TEMPORARY_DIRECTORY, '.vagrant')
+    mkdir(vagrant_directory)
+    assert VagrantSegment.vagrant_directory_exists(TEMPORARY_DIRECTORY)
+    rmdir(vagrant_directory)
+    assert not VagrantSegment.vagrant_directory_exists(TEMPORARY_DIRECTORY)
 
 
 def test_vagrant_status() -> None:
     with pytest.raises(CommandFailed):
-        VagrantSegment.vagrant_status('/tmp')
+        VagrantSegment.vagrant_status(TEMPORARY_DIRECTORY)
+
+
+def test_remove_test_directory() -> None:
+    rmdir(TEMPORARY_DIRECTORY)
+    assert not exists(TEMPORARY_DIRECTORY)

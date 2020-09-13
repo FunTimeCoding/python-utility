69c64e11135c5c5238b12e8ea1147d3f7be21e21
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-14 01:36:36 +0200

    split test into with and without putput

--- tests/test_command_process.py
+++ tests/test_command_process.py
@@ -16,12 +16,24 @@ def test_command_process(capfd) -> None:
     assert standard_error == ''
 
 
-def test_command_fails() -> None:
+def test_command_fails_with_output() -> None:
     with pytest.raises(CommandFailed) as exception:
-        CommandProcess(arguments=['tests/fixture/command-fails.sh'])
+        CommandProcess(arguments=['tests/fixture/fails-with-output.sh'])
 
     assert 'test stdout' in str(exception.value)
-    assert 'tests/fixture/command-fails.sh' == exception.value.get_command()
+    assert exception.value.get_command() == 'tests/fixture/fails-with-output.sh'
     assert exception.value.get_return_code() == 1
     assert exception.value.get_standard_output() == 'test stdout'
     assert exception.value.get_standard_error() == 'test stderr'
+
+
+def test_command_fails_without_output() -> None:
+    with pytest.raises(CommandFailed) as exception:
+        CommandProcess(arguments=['tests/fixture/fails-without-output.sh'])
+
+    assert 'CommandFailed' in str(exception.value)
+    assert exception.value.get_command() == \
+           'tests/fixture/fails-without-output.sh'
+    assert exception.value.get_return_code() == 1
+    assert exception.value.get_standard_output() == ''
+    assert exception.value.get_standard_error() == ''

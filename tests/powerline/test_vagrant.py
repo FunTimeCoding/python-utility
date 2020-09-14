08c10aed48d59e7dc562df05da259aec5c1bfd87
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-14 02:21:38 +0200

    test callable case where status returns correctly

--- tests/powerline/test_vagrant.py
+++ tests/powerline/test_vagrant.py
@@ -1,5 +1,6 @@
-from os import mkdir, rmdir, getcwd
+from os import mkdir, rmdir, getcwd, remove
 from os.path import join, exists
+from shutil import rmtree
 
 import pytest
 
@@ -16,6 +17,7 @@ TEMPORARY_DIRECTORY = '/tmp/python_utility'
 
 
 def test_create_test_directory() -> None:
+    rmtree(TEMPORARY_DIRECTORY)
     mkdir(TEMPORARY_DIRECTORY)
     assert exists(TEMPORARY_DIRECTORY)
 
@@ -25,17 +27,50 @@ def test_vagrant_file_exists() -> None:
     assert not VagrantSegment.vagrant_file_exists(TEMPORARY_DIRECTORY)
 
 
-def test_callable() -> None:
+def test_vagrant_status_raises_on_empty_directory() -> None:
+    with pytest.raises(CommandFailed):
+        VagrantSegment.vagrant_status(TEMPORARY_DIRECTORY)
+
+
+def test_callable_no_vagrant_directory() -> None:
     segment = VagrantSegment()
-    power_line = None
     segment_info = {
         'shortened_path': TEMPORARY_DIRECTORY
     }
-    create_watcher = None
-    assert segment(power_line, segment_info, create_watcher) == [{
+    assert segment(None, segment_info, None) == [{
         'contents': '.vagrant directory not found',
         'highlight_groups': ['information:regular'],
     }]
+
+
+def test_callable_no_vagrant_file() -> None:
+    segment = VagrantSegment()
+    segment_info = {
+        'shortened_path': TEMPORARY_DIRECTORY
+    }
+    vagrant_directory = join(TEMPORARY_DIRECTORY, '.vagrant')
+    mkdir(vagrant_directory)
+    assert segment(None, segment_info, None) == [{
+        'contents': 'Vagrantfile not found',
+        'highlight_groups': ['information:regular'],
+    }]
+    rmdir(vagrant_directory)
+
+
+def test_callable_vagrant_file_and_directory_exist() -> None:
+    segment = VagrantSegment()
+    segment_info = {
+        'shortened_path': TEMPORARY_DIRECTORY
+    }
+    vagrant_directory = join(TEMPORARY_DIRECTORY, '.vagrant')
+    mkdir(vagrant_directory)
+    with open(join(TEMPORARY_DIRECTORY, 'Vagrantfile'), 'w') as fp:
+        pass
+    assert segment(None, segment_info, None) == [{
+        'contents': 'not created',
+        'highlight_groups': ['information:regular'],
+    }]
+    rmtree(vagrant_directory)
 
 
 def test_vagrant_directory_exists() -> None:
@@ -46,11 +81,6 @@ def test_vagrant_directory_exists() -> None:
     assert not VagrantSegment.vagrant_directory_exists(TEMPORARY_DIRECTORY)
 
 
-def test_vagrant_status() -> None:
-    with pytest.raises(CommandFailed):
-        VagrantSegment.vagrant_status(TEMPORARY_DIRECTORY)
-
-
 def test_remove_test_directory() -> None:
-    rmdir(TEMPORARY_DIRECTORY)
+    rmtree(TEMPORARY_DIRECTORY)
     assert not exists(TEMPORARY_DIRECTORY)

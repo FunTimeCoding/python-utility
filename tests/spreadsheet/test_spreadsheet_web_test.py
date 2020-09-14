df9a997213bf3bfde6b6e9b6bc046d8aa8712f3b
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-14 03:16:58 +0200

    add a cherrypy test case

---
+++ tests/spreadsheet/test_spreadsheet_web_test.py
@@ -0,0 +1,30 @@
+import cherrypy
+from cherrypy.test import helper
+
+from python_utility.spreadsheet.spreadsheet_service import SpreadsheetService
+
+
+class SpreadsheetWebTest(helper.CPWebCase):
+    @staticmethod
+    def setup_server():
+        cherrypy.tree.mount(SpreadsheetService(), '/', {})
+
+    def test_status(self):
+        self.getPage("/status")
+        self.assertStatus('200 OK')
+
+    def test_spreadsheet_search_missing(self):
+        body = '{}'
+        headers = [('Content-Type', 'application/json'),
+                   ('Content-Length', str(len(body)))]
+        self.getPage("/spreadsheet", method='POST', headers=headers, body=body)
+        self.assertBody('"search missing"')
+        self.assertStatus('200 OK')
+
+    def test_spreadsheet_replace_missing(self):
+        body = '{"search": "test"}'
+        headers = [('Content-Type', 'application/json'),
+                   ('Content-Length', str(len(body)))]
+        self.getPage("/spreadsheet", method='POST', headers=headers, body=body)
+        self.assertBody('"replace missing"')
+        self.assertStatus('200 OK')

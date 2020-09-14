82e2c54f59815ff0baa17fe3448fbd1f70892aaf
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-14 03:25:54 +0200

    test run method

--- tests/test_user_interface.py
+++ tests/test_user_interface.py
@@ -3,4 +3,4 @@ from python_utility.user_interface import UserInterface
 
 def test_user_interface() -> None:
     user_interface = UserInterface(screen=None)
-    assert user_interface is not None
+    assert user_interface.run() == 1

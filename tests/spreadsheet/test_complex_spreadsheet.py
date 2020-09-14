1478b7ffde8afdd3bdc8854c768b63560f384f2d
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-14 03:01:17 +0200

    test print_rows method

--- tests/spreadsheet/test_complex_spreadsheet.py
+++ tests/spreadsheet/test_complex_spreadsheet.py
@@ -3,3 +3,17 @@ from python_utility.spreadsheet.complex_spreadsheet import ComplexSpreadsheet
 
 def test_complex_spreadsheet() -> None:
     assert ComplexSpreadsheet().identifier != ''
+
+
+def test_print_rows(capfd) -> None:
+    ComplexSpreadsheet.print_rows([['a1', 'a2'], ['b1', 'b2']])
+    standard_output, standard_error = capfd.readouterr()
+    assert standard_output.strip() == 'a1,a2\nb1,b2'
+    assert standard_error.strip() == ''
+
+
+def test_print_rows_no_data(capfd) -> None:
+    ComplexSpreadsheet.print_rows([])
+    standard_output, standard_error = capfd.readouterr()
+    assert standard_output.strip() == 'No data found.'
+    assert standard_error.strip() == ''

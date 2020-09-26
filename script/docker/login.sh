be4cd6ad1387d2bfa6b3f8ab8eb230a2f98b9b77
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-26 19:57:18 +0200

    login with password argument does not work because it requires a tty, try the password-stdin argument

--- script/docker/login.sh
+++ script/docker/login.sh
@@ -2,4 +2,4 @@
 
 # shellcheck source=/dev/null
 . "${HOME}/.virtualization-tools.sh"
-docker login --username "${PRIVATE_REGISTRY_USERNAME}" --password "${PRIVATE_REGISTRY_PASSWORD}" "${PRIVATE_REGISTRY_SERVER}"
+echo "${PRIVATE_REGISTRY_PASSWORD}" | docker login --username "${PRIVATE_REGISTRY_USERNAME}" --password-stdin "${PRIVATE_REGISTRY_SERVER}"

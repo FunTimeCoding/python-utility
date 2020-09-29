fbf1ad546c8a4d4cc0e3028d94eb01e839885480
Alexander Reitzel <funtimecoding@gmail.com>
2020-09-30 00:25:56 +0200

    clean up better

--- script/clean.sh
+++ script/clean.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -e
 
-rm -rf build
+DIRECTORY=$(dirname "${0}")
+SCRIPT_DIRECTORY=$(
+    cd "${DIRECTORY}" || exit 1
+    pwd
+)
+# shellcheck source=/dev/null
+. "${SCRIPT_DIRECTORY}/../../configuration/project.sh"
+rm -rf build .pytest_cache tests/.pytest_cache .scannerwork .mypy_cache "${PROJECT_NAME_UNDERSCORE}/build.py"
